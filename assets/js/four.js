d10926180ecbc31d6fca79fd801573b1c8598d0a
MauRuRo <m.rubenderoos@gmail.com>
2020-05-24 18:19:56 +0000

    added comments explaining newly added code

--- assets/js/four.js
+++ assets/js/four.js
@@ -4,8 +4,8 @@ $(document).ready(function () {
   var j = 0;                                    //setting new counter for checking player clicks with game array
   var i = 0;                                    //setting counter for looping through array and lighting up the board
   var demo = 0;                                 //setting a variable that determines weather a demo is in proces (demo = 1) or not; prevents player from clicking while demo is in proces. 
-  var turn = 0;
-  var turner;
+  var turn = 0;                                 //determines turning challenge is inactive
+  var turner;                                   //creates variable to contain for setInterval function
   $(".circle2").hide();                         //hides the circular edge that indicates that it's the players turn
    $("#gameover").hide();                       //hides game over indicator
     $(".aboutfield").hide();                    //hides aboutfield information div
@@ -27,7 +27,7 @@ $(document).ready(function () {
     .click(function () {                        //applies to all colored circle slices
       if (game.length == 0 || demo == 1) {      //game hasn't started or demo is in proces: do nothing on click
       } else {
-         var color = $(this).attr("class").split(' ')[0];   
+         var color = $(this).attr("class").split(' ')[0];   //gets first class name
         var index;
         switch (color) {
           case "red-4-inner":
@@ -52,14 +52,14 @@ $(document).ready(function () {
           $(this).fadeIn(500);
           j = j + 1;                                    //increments j counter: for the next click it will check the next item in the game array
     } else {
-        angle = 0;
-        turn = 0;
-        clearInterval(turner)
-        $(".circle").css("transform", "rotate(0deg)");
-       $(".turnchallenge").html("Difficulty");
+        angle = 0;                                      //sets start angle for turnchallenge at 0
+        turn = 0;                                       //determines turn challenge is inactive.
+        clearInterval(turner)                           //clears the setInterval function which stops the rotation.
+        $(".circle").css("transform", "rotate(0deg)");  //makes sure the circle and it's (text) contents is right side up
+       $(".turnchallenge").html("Difficulty");          //sets the level header back to "Difficulty"
         $("#gameover").fadeIn(1000);                    //player made a mistake: game over is shown, prompt for new game
-        $(".circle2").hide();
-        setTimeout(function(){
+        $(".circle2").hide();                           //hides "player turn" indicator circle
+        setTimeout(function(){ 
         $("#counter").hide();                           
         $("#gameover").fadeOut(1000);
         $("#startnewgame4").fadeIn(1000);  
@@ -82,7 +82,6 @@ $(document).ready(function () {
     setTimeout(function () {                            //sets time out for demo, because we want to wait until start button has faded and give the player a second to concentrate.
       let colorindex = Math.ceil(Math.random() * 4);    //selects a random number between 1 and 4
       game.push(colorindex);                            //pushes number in to game array     
-
       switch (colorindex) {                             //matches number to game tile and toggleFades it.
         case 1:
           $(".red-4-inner").fadeOut(1000);
@@ -102,7 +101,7 @@ $(document).ready(function () {
           break;
       }
         setTimeout(function(){                          //sets time out before "player turn" circle shows, so that it shows after the tile has faded back in.
-      $(".circle2").show();
+      $(".circle2").show();                             //shows "player turn" indicator circle
     $(".circle").children().children().addClass("point");                //changes cursor properties to indicate tiles are clickable                            
 
       demo = 0;
@@ -189,27 +188,27 @@ $(document).ready(function () {
     }, 1700);
   };
 
-$(".turnchallenge").mouseover(function () {
-        if (turn == 0){
+$(".turnchallenge").mouseover(function () {                     
+        if (turn == 0){                                         //if rotation is inactive then indicate the button on mouseover
  $(".turnchallenge").html("Press for secret challenge")}
 });
-$(".turnchallenge").mouseleave(function() {
+$(".turnchallenge").mouseleave(function() {                     //if rotation is inactive then revert to "difficulty" on mouseleave
     if (turn == 0){
     $(".turnchallenge").html("Difficulty")}
 });
- $(".turnchallenge").click(function () {
- $(".turnchallenge").html("Press to stop rotation");
-    if (turn ==  0) {
+ $(".turnchallenge").click(function () {                        
+ $(".turnchallenge").html("Press to stop rotation");            //changes header text to Press stop rotation after it's been clicked.
+    if (turn ==  0) {                                           //if rotation is inactive then start rotation.
      var angle = 0;
      $(".circle").addClass("rotate");
-turner = setInterval(function() {
+turner = setInterval(function() {                               //function found at: http://jsfiddle.net/YgBMa/1
     $(".rotate")
         .css('-webkit-transform', 'rotate('+angle+'deg)')
         .css('-moz-transform', 'rotate('+angle+'deg)')
         .css('-ms-transform', 'rotate('+angle+'deg)');
     angle++; angle++; angle++;
 }, 50);
-turn = 1} else {
+turn = 1} else {                                                //determine rotation is active | if active then set to inactive.
 angle = 0;
 turn = 0;
 clearInterval(turner)
